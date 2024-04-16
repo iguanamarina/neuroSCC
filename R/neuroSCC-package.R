f672edd08f9286372cb06df8d3a5b9270226450d
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2024-04-16 11:13:55 +0200

    Update

--- R/neuroSCC-package.R
+++ R/neuroSCC-package.R
@@ -1,12 +1,11 @@
-#' @keywords internal
-#' @name neuroSCC-package
 #' @title neuroSCC: Estimation of SCC's for PET Neuroimaging Data
 #' @description
 #' Este paquete proporciona funciones auxiliares para calcular Corredores de Confianza Simultáneos (SCCs)
-#' en datos de neuroimagen PET. Incluye funciones para cargar datos neuroimagen y demográficos en el formato estándar requerido,
+#' en datos de neuroimagen PET. Incluye funciones para cargar datos de neuroimagen y demográficos en el formato estándar requerido,
 #' y asiste en varias tareas a lo largo del proceso. Este paquete está diseñado para ayudar con el proceso
 #' de replicación de un artículo fundamental de mi tesis "Development of statistical methods for neuroimage data analysis
 #' towards early diagnostic of neurodegenerative diseases" y se sigue mejor utilizando los scripts disponibles en
 #' \url{https://github.com/iguanamarina/SCCneuroimage}. Este sirve como un paquete de soporte.
-#' \figure{figures/logo.png}{options: width=120}
-"_PACKAGE"
+#' \figure{logo.png}{options: width=120}
+#' @name neuroSCC
+NULL
