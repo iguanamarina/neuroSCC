fb8d3ea45cbeb786e095790df9132d077da3bb8b
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2024-07-08 10:49:57 +0100

    databaseCreator updates

--- R/databaseCreator.R
+++ R/databaseCreator.R
@@ -1,15 +1,18 @@
-#' Create a database of processed PET image data with the appropiate format
+#' Create a database of processed PET image data with the appropriate format
 #'
 #' @description
 #' This function automates the processing of PET images based on a specified file name pattern within a working directory.
 #' It reads each file matching the pattern, processes it using `neuroSCC::neuroCleaner`, and compiles the results into a comprehensive data frame.
+#' The function can handle both control and pathological data based on the `control` parameter.
 #'
 #' @param pattern `character`, a regular expression pattern that specifies which files to process.
+#' @param control `logical`, if `TRUE`, the function processes control group images and if `FALSE`, it processes pathological group images. Default is `TRUE`.
 #' @return A `data.frame` that aggregates processed data from each image. Each row represents data from one image, including subject numbers and image data.
 #' @details The function first checks if there are files matching the pattern in the working directory.
 #' If no files are found, it throws an error. Otherwise, it processes each file individually, extracts necessary data,
 #' and appends it to a growing database. The function leverages `neuroSCC::neuroCleaner` for data processing.
-#' Each file's subject number is extracted from its name using regular expressions.
+#' Each file's subject number is extracted from its name using regular expressions. Depending on the `control` parameter,
+#' the database will have either a `CN_number` column for control data or an `AD_number` column for pathological data.
 #'
 #' @examples
 #' # Set the working directory where your PET images are stored
@@ -18,14 +21,17 @@
 #' # Define the pattern for file names to process
 #' pattern <- "^masked_swwwC\\d+_tripleNormEsp_w00_rrec_OSEM3D_32_it1.nii"
 #'
-#' # Create the database
-#' database_CN <- databaseCreator(pattern)
+#' # Create the database for control group images
+#' database_CN <- databaseCreator(pattern, control = TRUE)
+#'
+#' # Create the database for pathological group images
+#' database_AD <- databaseCreator(pattern, control = FALSE)
 #'
 #' @export
 #' @seealso \link[neuroSCC]{neuroCleaner} for the underlying image processing.
 #'
 
-databaseCreator <- function(pattern) {
+databaseCreator <- function(pattern, control = TRUE) {
   # Get the list of files matching the pattern
   files <- list.files(pattern = pattern, full.names = TRUE)
 
@@ -34,23 +40,31 @@ databaseCreator <- function(pattern) {
     stop("Error: No files with this pattern in this working directory.")
   }
 
-  # Initialize the database for Control group images
-  database_CN <- data.frame(CN_number = integer(), z = integer(), x = integer(), y = integer(), pet = numeric())
+  # Initialize the database for Control or Pathological group images
+  if (control) {
+    database <- data.frame(CN_number = integer(), z = integer(), x = integer(), y = integer(), pet = numeric())
+    group_label <- "Control"
+    number_label <- "CN_number"
+  } else {
+    database <- data.frame(AD_number = integer(), z = integer(), x = integer(), y = integer(), pet = numeric())
+    group_label <- "Pathological"
+    number_label <- "AD_number"
+  }
 
   # Process each file and append to the database
   for(file in files) {
     # Use neuroSCC::neuroCleaner to process the file
     temporal <- neuroSCC::neuroCleaner(file)
     # Extract the number from the file name using a regular expression
-    CN_number <- sub("masked_swwwC(\\d+)_.*", "\\1", basename(file))
-    # Print the current control number being processed
-    print(paste("Processing Control Nº", CN_number))
+    number <- sub("masked_swwwC(\\d+)_.*", "\\1", basename(file))
+    # Print the current control or pathological number being processed
+    print(paste("Processing", group_label, "Nº", number))
     # Repeat the number for each row in the temporal data frame
-    temporal$CN_number <- rep(CN_number, nrow(temporal))
+    temporal[[number_label]] <- rep(number, nrow(temporal))
     # Append the processed data to the main database
-    database_CN <- rbind(database_CN, temporal)
+    database <- rbind(database, temporal)
   }
 
   # Return the complete database
-  return(database_CN)
+  return(database)
 }
